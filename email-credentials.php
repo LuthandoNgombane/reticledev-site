aebdb19dc82db51965f63b88d47f1c083e3b4941
Luthando Ngombane <luthandongombane@gmail.com>
2025-09-18 23:06:23 +0200

    Update email-credentials.php

--- email-credentials.php
+++ email-credentials.php
@@ -1,6 +1,1 @@
-<?php
-define('SMTP_HOST', 'mail.reticledev.co.za');
-define('SMTP_USERNAME', 'enquiries@reticledev.co.za');
-define('SMTP_PASSWORD', 'UmbrellaDev2025');
-define('SMTP_PORT', 465);
-?>
+
